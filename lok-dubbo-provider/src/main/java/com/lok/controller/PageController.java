fbcde0d9ca745e9fb0682d823479b0718be49206
coxujiale <574262853@qq.com>
2017-10-10 18:03:01 +0800

    初步搭建spring+springmvc项目雏形

---
+++ lok-dubbo-provider/src/main/java/com/lok/controller/PageController.java
@@ -0,0 +1,22 @@
+package com.lok.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Description 
+ * @date 2017年10月10日
+ */
+
+@Controller
+@RequestMapping("pages")
+public class PageController {
+
+	@RequestMapping(value="{folderName}/{pageName}")
+	public String toPage(
+			@PathVariable("folderName")String folderName,
+			@PathVariable("pageName")String pageName) {
+		return folderName + "/" + pageName;
+	}
+}
